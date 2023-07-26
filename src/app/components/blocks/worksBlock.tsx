d4f4b26dff1dba6e6f12a220e95dab84368ce196
kentsuma <kentsumalinog.work@gmail.com>
2023-07-26 08:34:42 +0800

    Added dots 1 and 2 for reference in worksBlock

--- src/app/components/blocks/worksBlock.tsx
+++ src/app/components/blocks/worksBlock.tsx
@@ -29,6 +29,37 @@ function WorksBlock({
           >
             {myeWorking.headerWorks}
           </h1>
+          {/* DOT 1 */}
+          <div className="dot-1-container">
+            <div className="dot-1"></div>
+            <div className="dot-1-round"></div>
+            <div className="dot-1-line"></div>
+
+            <div className="popup-content1">
+              <h2>Security</h2>
+              <ul>
+                <li>Native Security capabilities including firewalls</li>
+                <li>WAF</li>
+                <li>Encryption</li>
+              </ul>
+            </div>
+          </div>
+          {/* DOT 2 */}
+          <div className="dot-2-container">
+            <div className="dot-2"></div>
+            <div className="dot-2-round"></div>
+            <div className="dot-2-line"></div>
+
+            <div className="popup-content2">
+              <h2>Compute</h2>
+              <ul>
+                <li>
+                  Powered by latest intel (TM) generation processors for maximum
+                  performance
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
         <div
           className="h-[30vh] sm:h-[35vh] md:h-[40vh] lg:h-[50vh]"
