3c358a1c52b65fde6195d69ba5cca4b586730a51
kentsuma <kentsumalinog.work@gmail.com>
2023-07-26 16:22:05 +0800

    Make hover for worksBlock dynamic data

--- src/app/components/blocks/worksBlock.tsx
+++ src/app/components/blocks/worksBlock.tsx
@@ -29,6 +29,19 @@ function WorksBlock({
           >
             {myeWorking.headerWorks}
           </h1>
+        </div>
+        <div
+          className="h-[30vh] sm:h-[35vh] md:h-[40vh] lg:h-[50vh]"
+          style={{
+            backgroundImage: `url(${myeWorking.imageWorking.sourceUrl})`,
+            backgroundSize: "contain",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            position: "relative",
+            height: "500px",
+            width: "100%",
+          }}
+        >
           {/* DOT 1 */}
           <div className="dot-1-container">
             <div className="dot-1"></div>
@@ -36,11 +49,13 @@ function WorksBlock({
             <div className="dot-1-line"></div>
 
             <div className="popup-content1">
-              <h2>Security</h2>
+              <h2>{myeWorking.workingItem[0].title}</h2>
               <ul>
-                <li>Native Security capabilities including firewalls</li>
-                <li>WAF</li>
-                <li>Encryption</li>
+                {myeWorking.workingItem[0].worksDescription.map(
+                  (description, idx) => (
+                    <li key={idx}>{description.workingDescription}</li>
+                  ),
+                )}
               </ul>
             </div>
           </div>
@@ -51,12 +66,13 @@ function WorksBlock({
             <div className="dot-2-line"></div>
 
             <div className="popup-content2">
-              <h2>Compute</h2>
+              <h2>{myeWorking.workingItem[1].title}</h2>
               <ul>
-                <li>
-                  Powered by latest intel (TM) generation processors for maximum
-                  performance
-                </li>
+                {myeWorking.workingItem[1].worksDescription.map(
+                  (description, idx) => (
+                    <li key={idx}>{description.workingDescription}</li>
+                  ),
+                )}
               </ul>
             </div>
           </div>
@@ -67,11 +83,13 @@ function WorksBlock({
             <div className="dot-3-line"></div>
 
             <div className="popup-content3">
-              <h2>Network</h2>
+              <h2>{myeWorking.workingItem[2].title}</h2>
               <ul>
-                <li>Secured Multi-tenant networking capabilities</li>
-                <li>Customer Datacenter Connectivity (VPN)</li>
-                <li>Advanced Load Balancing</li>
+                {myeWorking.workingItem[2].worksDescription.map(
+                  (description, idx) => (
+                    <li key={idx}>{description.workingDescription}</li>
+                  ),
+                )}
               </ul>
             </div>
           </div>
@@ -82,11 +100,13 @@ function WorksBlock({
             <div className="dot-4-line"></div>
 
             <div className="popup-content4">
-              <h2>Hosted In-Country</h2>
+              <h2>{myeWorking.workingItem[3].title}</h2>
               <ul>
-                <li>Datacenter in the Philippines</li>
-                <li>Customer Datacenter Connectivity (VPN)</li>
-                <li>Certified Tier III Datacenter</li>
+                {myeWorking.workingItem[3].worksDescription.map(
+                  (description, idx) => (
+                    <li key={idx}>{description.workingDescription}</li>
+                  ),
+                )}
               </ul>
             </div>
           </div>
@@ -95,28 +115,18 @@ function WorksBlock({
             <div className="dot-5"></div>
             <div className="dot-5-round"></div>
             <div className="dot-5-line"></div>
-
             <div className="popup-content5">
-              <h2>Platform Capabilities</h2>
+              <h2>{myeWorking.workingItem[4].title}</h2>
               <ul>
-                <li>Platform Capabilities</li>
-                <li>Database-as-a-Service</li>
-                <li>Data Warehouse</li>
-                <li>Secure Storage</li>
+                {myeWorking.workingItem[4].worksDescription.map(
+                  (description, idx) => (
+                    <li key={idx}>{description.workingDescription}</li>
+                  ),
+                )}
               </ul>
             </div>
           </div>
         </div>
-        <div
-          className="h-[30vh] sm:h-[35vh] md:h-[40vh] lg:h-[50vh]"
-          style={{
-            backgroundImage: `url(${myeWorking.imageWorking.sourceUrl})`,
-            backgroundSize: "contain",
-            backgroundPosition: "center",
-            backgroundRepeat: "no-repeat",
-            position: "relative",
-          }}
-        ></div>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:hidden gap-3 mt-5 lg:mt-0 text-white">
           {myeWorking.workingItem.map((workItem, index) => (
