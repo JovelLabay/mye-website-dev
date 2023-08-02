ed272e2fdf0b11a6c8d012b6df8a3c7c388b4ae0
jovellabay <jovellabay@gmail.com>
2023-08-02 16:23:55 +0800

    1. Add get in touch functionality

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -1,9 +1,41 @@
+import axios from "axios";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest) {
-    
-    return NextResponse.json({
-        message: "Hello World"
-    });
-    
+export async function POST(req: Request, res: Response, next: NextRequest) {
+  const { emailAddress, name, message } = await req.json();
+
+  const recepients =
+    (process.env.EMAIL_RECEPIENTS &&
+      (JSON.parse(process.env.EMAIL_RECEPIENTS) as string[])) ||
+    undefined;
+
+  // EMAIL CONTENT
+  const content = {
+    personalizations: [
+      {
+        to: recepients?.map((recepient: string) => ({ email: recepient })),
+        subject: name || "No Subject",
+      },
+    ],
+    from: { email: emailAddress },
+    content: [{ type: "text/html", value: message }],
+  };
+
+  // AXIOS CONFIG
+  const options = {
+    method: "POST",
+    url: process.env.SEND_GRID_EMAIL_URL,
+    headers: {
+      "content-type": "application/json",
+      "X-RapidAPI-Key": process.env.RAPID_API_KEY,
+      "X-RapidAPI-Host": process.env.RAPID_API_HOST,
+    },
+    data: JSON.stringify(content),
+  };
+
+  const response = await axios.request(options);
+
+  const { status, statusText } = response;
+
+  return NextResponse.json({ data: { status, statusText } });
 }
