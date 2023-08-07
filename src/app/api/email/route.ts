5c4a5489ee95bead53eb8149158bb2b2396e4171
jovellabay <jovellabay@gmail.com>
2023-08-07 17:09:53 +0800

    Email API updated

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -9,33 +9,68 @@ export async function POST(req: Request, res: Response, next: NextRequest) {
       (JSON.parse(process.env.EMAIL_RECEPIENTS) as string[])) ||
     undefined;
 
-  // EMAIL CONTENT
-  const content = {
-    personalizations: [
-      {
-        to: recepients?.map((recepient: string) => ({ email: recepient })),
-        subject: name || "No Subject",
-      },
-    ],
-    from: { email: emailAddress },
-    content: [{ type: "text/html", value: message }],
-  };
+  // // EMAIL CONTENT
+  // const content = {
+  //   personalizations: [
+  //     {
+  //       to: recepients?.map((recepient: string) => ({ email: recepient })),
+  //       subject: name || "No Subject",
+  //     },
+  //   ],
+  //   from: { email: emailAddress },
+  //   content: [{ type: "text/html", value: message }],
+  // };
 
-  // AXIOS CONFIG
+  // // AXIOS CONFIG
+  // const options = {
+  //   method: "POST",
+  //   url: process.env.SEND_GRID_EMAIL_URL,
+  //   headers: {
+  //     "content-type": "application/json",
+  //     "X-RapidAPI-Key": process.env.RAPID_API_KEY,
+  //     "X-RapidAPI-Host": process.env.RAPID_API_HOST,
+  //   },
+  //   data: JSON.stringify(content),
+  // };
+
+  // const response = await axios.request(options);
+
+  // const { status, statusText } = response;
+
+  // return NextResponse.json({
+  //   data: recepients?.map((recepient: string) => ({ email: recepient })),
+  // });
+
   const options = {
     method: "POST",
-    url: process.env.SEND_GRID_EMAIL_URL,
+    url: process.env.SEND_GRID_EMAIL_URL as string,
     headers: {
       "content-type": "application/json",
-      "X-RapidAPI-Key": process.env.RAPID_API_KEY,
-      "X-RapidAPI-Host": process.env.RAPID_API_HOST,
+      "X-RapidAPI-Key": process.env.RAPID_API_KEY as string,
+      "X-RapidAPI-Host": process.env.RAPID_API_HOST as string,
     },
-    data: JSON.stringify(content),
+    data: {
+      personalizations: [
+        {
+          to: recepients?.map((recepient: string) => ({ email: recepient })),
+          subject: name || "No Subject",
+        },
+      ],
+      from: {
+        email: emailAddress,
+      },
+      content: [
+        {
+          type: "text/html",
+          value: message,
+        },
+      ],
+    },
   };
 
   const response = await axios.request(options);
 
-  const { status, statusText } = response;
-
-  return NextResponse.json({ data: { status, statusText } });
+  return NextResponse.json({
+    data: response.data,
+  });
 }
