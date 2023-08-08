078312006e495b0ec204a267725dd96a49190a5c
kentsuma <kentsumalinog.work@gmail.com>
2023-08-08 16:11:31 +0800

    Fix industry solutions

--- src/app/components/blocks/industryBlock.tsx
+++ src/app/components/blocks/industryBlock.tsx
@@ -34,63 +34,62 @@ function IndustryBlock({
       </div>
 
       <div className="grid grid-cols-1 lg:grid-cols-8 mt-5">
-        {industrySolutions.solutionItem.map((solution, index) => (
-          <div
-            onClick={() => {
-              setActive(index);
-            }}
-            key={index}
-            className={classNames(
-              "h-[35vh] lg:h-[70vh]",
-              active === null
-                ? "lg:col-span-2"
-                : active === index
-                ? "h-[60vh] lg:col-span-5"
-                : "lg:col-span-1",
-            )}
-            style={{
-              backgroundImage: `url(${
-                solution.backgroundImage?.sourceUrl || ""
-              })`,
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-              backgroundRepeat: "no-repeat",
-              position: "relative",
-            }}
-          >
-            <div id="industry-boxes">
-              <h5
-                className={classNames(
-                  "font-semibold leading-7 text-white w-full text-center",
-                  active === null
-                    ? ""
-                    : active === index
-                    ? ""
-                    : "lg:mb-20 lg:rotate-[-90deg]",
+        {industrySolutions.solutionItem &&
+          industrySolutions.solutionItem.map((solution, index) => (
+            <div
+              onClick={() => {
+                setActive(index);
+              }}
+              key={index}
+              className={classNames(
+                "h-[35vh] lg:h-[70vh]",
+                active === null
+                  ? "lg:col-span-2"
+                  : active === index
+                  ? "h-[60vh] lg:col-span-5"
+                  : "lg:col-span-1",
+              )}
+              style={{
+                backgroundImage: `url(${solution.backgroundImage?.sourceUrl})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+                position: "relative",
+              }}
+            >
+              <div id="industry-boxes">
+                <h5
+                  className={classNames(
+                    "font-semibold leading-7 text-white w-full text-center",
+                    active === null
+                      ? ""
+                      : active === index
+                      ? ""
+                      : "lg:mb-20 lg:rotate-[-90deg]",
+                  )}
+                >
+                  {solution.titleItems}
+                </h5>
+                {active === index && (
+                  <ul className="text-white p-0">
+                    {solution.items?.map((theItem, index) => (
+                      <li key={index}>{theItem?.descriptionItem}</li>
+                    ))}
+                  </ul>
                 )}
-              >
-                {solution.titleItems}
-              </h5>
-              {active === index && (
-                <ul className="text-white p-0">
-                  {solution.items?.map((theItem, index) => (
-                    <li key={index}>{theItem?.descriptionItem}</li>
-                  ))}
-                </ul>
-              )}
 
-              {active === null && (
-                <ul className="text-white p-0 mt-3">
-                  {solution.items?.map((theItem, index) => (
-                    <li key={index} className="hidden" id="hover-items">
-                      {theItem?.descriptionItem}
-                    </li>
-                  ))}
-                </ul>
-              )}
+                {active === null && (
+                  <ul className="text-white p-0 mt-3">
+                    {solution.items?.map((theItem, index) => (
+                      <li key={index} className="hidden" id="hover-items">
+                        {theItem?.descriptionItem}
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
   );
