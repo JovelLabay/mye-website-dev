66d6fb5032d87a8103d0cd7a179d7b58115f36b6
kentsuma <kentsumalinog.work@gmail.com>
2023-08-09 09:47:42 +0800

    Add carousel animation (still fixing)

--- src/app/components/blocks/carouselBlock.tsx
+++ src/app/components/blocks/carouselBlock.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Carousel from "react-bootstrap/Carousel";
+import { motion, AnimatePresence } from "framer-motion";
 
 function CarouselBlock({
   carouselItem,
@@ -20,9 +21,33 @@ function CarouselBlock({
     };
   }[];
 }) {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleSelect = (selectedIndex: number) => {
+    setActiveIndex(selectedIndex);
+  };
+
+  useEffect(() => {
+    // Reset animations when active index changes
+    carouselItem.forEach((_, index) => {
+      const elements = document.querySelectorAll(`.animation-element-${index}`);
+      elements.forEach((element) => {
+        const htmlElement = element as HTMLElement;
+        htmlElement.style.transform = "translateX(100px)";
+        htmlElement.style.opacity = "0";
+      });
+    });
+  }, [activeIndex, carouselItem]);
+
   return (
     <div>
-      <Carousel interval={2000} controls={false} fade={true}>
+      <Carousel
+        interval={2000}
+        controls={false}
+        fade={true}
+        activeIndex={activeIndex}
+        onSelect={handleSelect}
+      >
         {carouselItem.map((item, index) => (
           <Carousel.Item key={index}>
             <div
@@ -44,15 +69,33 @@ function CarouselBlock({
                 }}
               >
                 <div className="flex flex-col justify-center items-end h-full px-[20px] sm:px-[30px] md:px-[60px] lg:px-[120px]">
-                  <h1 className="font-bold text-[24px] sm:text-[28px] md:text-[34px] lg:text-[40px] ">
+                  <motion.h1
+                    key={index}
+                    initial={{ x: 100, opacity: 0 }}
+                    animate={{ x: 0, opacity: 1 }}
+                    transition={{ duration: 1 }}
+                    className="font-bold text-[24px] sm:text-[28px] md:text-[34px] lg:text-[40px] "
+                  >
                     {item.label}
-                  </h1>
-                  <p className="py-3">{item.description}</p>
-                  <a href={item.learnMore.url}>
+                  </motion.h1>
+                  <motion.p
+                    initial={{ x: 100, opacity: 0 }}
+                    animate={{ x: 0, opacity: 1 }}
+                    transition={{ duration: 1.2 }}
+                    className="py-3"
+                  >
+                    {item.description}
+                  </motion.p>
+                  <motion.a
+                    initial={{ x: 100, opacity: 0 }}
+                    animate={{ x: 0, opacity: 1 }}
+                    transition={{ duration: 1.4 }}
+                    href={item.learnMore.url}
+                  >
                     <button className="py-[5px] md:py-[8px] lg:py-[10px] px-[20px] sm:px-[24px] md:px-[30px] lg:px-[40px] rounded-full bg-white text-customViolet font-medium md:font-semibold">
                       Learn more
                     </button>
-                  </a>
+                  </motion.a>
                 </div>
               </div>
             </div>
