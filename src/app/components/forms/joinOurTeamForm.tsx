4958392bad5824024b4a9548579d704282953726
kentsuma <kentsumalinog.work@gmail.com>
2023-08-10 16:08:48 +0800

    Add validation for files

--- src/app/components/forms/joinOurTeamForm.tsx
+++ src/app/components/forms/joinOurTeamForm.tsx
@@ -38,20 +38,45 @@ function JoinOurTeamForm({
     mode: "onBlur",
     resolver: yupResolver(joinourTeamFormSchema),
   });
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [isRed, setIsRed] = useState(false);
+  const [fileError, setfileError] = useState(true);
 
   const onSubmit = (data: JoinOurTeamForm) => {
     console.log("Form data:", data);
+    console.log("TEST", selectedFile);
+    if (selectedFile === null) {
+      console.log("Hello");
+      setfileError(true);
+      setIsRed(true);
+      setErrorMessage("Please select a valid PDF file.");
+      return null;
+    }
+    setIsRed(false);
     setCurrentStatus((prev) => ({ ...prev, buttonStatus: true }));
     setTimeout(() => {
       console.log("Form data:", data);
-      // You can do further processing or API calls here
-
-      // After processing, reset the button status to false
       setCurrentStatus((prev) => ({ ...prev, buttonStatus: false }));
-      setCurrentStatus((prev) => ({ ...prev, feedback: true }));
+      setCurrentStatus((prev) => ({ ...prev, feedback: true, modal: false }));
+      reset();
     }, 2000);
 
     // You can do further processing or API calls here
+  };
+
+  const handleFileChange = (event: any) => {
+    const selected = event.target.files[0];
+
+    if (selected && selected.type === "application/pdf") {
+      setSelectedFile(selected);
+      setIsRed(false);
+      setErrorMessage("");
+    } else {
+      setSelectedFile(null);
+      setIsRed(true);
+      setErrorMessage("Please select a valid PDF file.");
+    }
   };
 
   return (
@@ -62,7 +87,7 @@ function JoinOurTeamForm({
       >
         <div className="fixed inset-0 bg-black/30 rounded" aria-hidden="true" />
 
-        <div className="fixed right-0 left-0 bottom-0 top-[80px] items-center justify-center p-4 overflow-y-auto h-auto">
+        <div className="fixed right-0 left-0 bottom-0 top-[80px] items-center justify-center p-4 overflow-y-auto h-auto z-10">
           <Dialog.Panel className="mx-auto max-w-lg rounded bg-white p-4 z-10 overflow-y-auto">
             <div
               className="text-black relative"
@@ -81,14 +106,20 @@ function JoinOurTeamForm({
                   <form className="mt-2" onSubmit={handleSubmit(onSubmit)}>
                     <div className="mt-4 max-w-[600px] mx-auto mb-10">
                       <label htmlFor="file" className="mb-2">
-                        Upload File
+                        Upload File <span className="text-red-400">*</span>
                       </label>
                       <input
                         type="file"
                         id="file"
                         accept="application/pdf"
                         className="rounded-lg p-2 bg-gray-100 text-black outline-none w-[220px] md:w-[350px] lg:w-fit"
+                        onChange={handleFileChange}
                       />
+                      {fileError && isRed && (
+                        <p className="bg-red-300 rounded font-light py-1 px-2 mt-2 text-white">
+                          {errorMessage}
+                        </p>
+                      )}
                     </div>
                     <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 max-w-[600px] mx-auto">
                       <div className="flex flex-col">
@@ -186,7 +217,7 @@ function JoinOurTeamForm({
       >
         <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
 
-        <div className="fixed inset-0 flex items-center justify-center p-4">
+        <div className="fixed inset-0 flex items-center justify-center p-4 z-20">
           <Dialog.Panel className="mx-auto max-w-lg rounded bg-white p-4">
             <div className="flex flex-col justify-center items-center gap-3">
               <h3 className="text-center flex justify-center items-center gap-3 my-3 text-[24px] sm:text-[26px] md:text-[28px] lg:text-[30px] font-bold text-customViolet">
